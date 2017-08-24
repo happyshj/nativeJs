b4a494201e6e06bdba95aa1bc7e579c7861952bf
Sun <Hejuan.Sun@spirent.com>
2017-08-24 17:03:24 +0800

    判断数组，函数，深度克隆

---
+++ task01/test.js
@@ -0,0 +1,11 @@
+// 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
+function uniqArray(arr) {
+    // your implement
+}
+
+// 使用示例
+var a = [1, 3, 5, 7, 5, 3];
+var b = uniqArray(a);
+console.log(b); // [1, 3, 5, 7]
+
+
---
+++ task01/util.js
@@ -0,0 +1,64 @@
+// 判断arr是否为一个数组，返回一个bool值
+function isArray(arr) {
+    // your implement
+    //return arr instanceof Array;
+    // return Array.isArray(arr);
+    return Object.prototype.toString.call(arr) === '[Object Array]';
+}
+
+// 判断fn是否为一个函数，返回一个bool值
+function isFunction(fn) {
+    // your implement
+    return typeof fn === "true";
+}
+
+
+// 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
+// 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
+function cloneObject(src) {
+    if (typeof src == "object"){
+        if (Object.prototype.toString.call(src) === '[object Array]'){
+            var temp = [];
+            src.forEach(function (item, index,array) {
+                temp.push(cloneObject(item));
+            });
+            return temp;
+        }else if (Object.prototype.toString.call(src) === '[object Object]'){
+            // 对象
+            var temp1 = {};
+            for(var item in src){
+                if (src.hasOwnProperty(item)){
+                    temp1[item] = cloneObject(src[item]);
+                }
+            }
+            return temp1;
+        }else if(Object.prototype.toString.call(src) === '[object Date]'){
+            return src;
+        }else {
+            // 其他情况
+            return src;
+        }
+    }else {
+        return src;
+    }
+}
+
+// 测试用例：
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a);      // 1
+console.log(tarObj.b.b1[0]);    // "hello"
