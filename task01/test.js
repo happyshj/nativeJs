60141fa9976425cd6c2c88ef87a8c6196eb47c56
Sun <Hejuan.Sun@spirent.com>
2017-08-29 13:47:30 +0800

    DOM

--- task01/test.js
+++ task01/test.js
@@ -1,28 +1,3 @@
-// 为element增加一个样式名为newClassName的新样式
-function addClass(element, newClassName) {
-    element.classList.add(newClassName);
-    return element;
-}
 
-// 移除element中的样式oldClassName
-function removeClass(element, oldClassName) {
-    element.classList.remove(oldClassName);
-    return element;
-}
 
-// 判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
-function isSiblingNode(element, siblingNode) {
-    let parentElement = element.parentNode;
-    let childrenElement = parentElement.children;
-    [].prototype.some.call(childrenElement, function (item, index, array) {
-        return item === siblingNode;
-    });
-}
 
-// 实现一个简单的Query
-function $(selector) {
-    let temp = selector.charAt(0);
-    if (temp == '#'){
-
-    }
-}
